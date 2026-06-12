5f958ad73db172541b333456aed397ca6ef7be76
Ahmed Ibrahiem <ahmedebrihem935@gmail.com>
2026-06-12 17:37:03 +0300

    refector: re-design favorite item and design the empty message

--- src/Pages/Wishlist/FavoriteItem.jsx
+++ src/Pages/Wishlist/FavoriteItem.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import { IoTrashOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import { formate_date } from "../../services/formatsDate";
+import { MdOutlineCalendarMonth } from "react-icons/md";
+import { useFavoriteContext } from "../../Context/favoriteMenuContext";
+import { useCartContext } from "../../Context/CartMenuContext";
 
-const FavoriteItem = ({productData}) => {
+const FavoriteItem = ({ productData }) => {
+  const { handleFavoriteItems } = useFavoriteContext();
   return (
-    <div>FavoriteItem</div>
-  )
-}
+    <div className={`${itemStyle}`}>
+      {/* Item Right */}
+      <div className="flex flex-col w-full xs:w-auto xs:flex-row  xs:items-center gap-5 relative ">
+        {/* Trash Item */}
+        <button
+          onClick={() => handleFavoriteItems(productData)}
+          className="p-2 w-fit absolute top-5 xs:top-0 left-5 xs:left-0 xs:relative rounded-full bg-gray-light text-gray flex-center hover:text-red-500!"
+        >
+          <IoTrashOutline size={20} />
+        </button>
+        {/* Item Image */}
+        <div className="img-box flex-center h-50 xs:h-25 w-full xs:w-22.5 rounded-sm bg-gray-light/60">
+          <img
+            src={productData.thumbnail}
+            className="max-w-[80%] max-h-[80%]"
+            alt=""
+          />
+        </div>
+        {/* Item Info */}
+        <div className="flex-start-col gap-2.5 text-sm">
+          {/* Item name */}
+          <Link
+            to={`/product_detials/${productData.slug}`}
+            className=" font-semibold hover:text-orange!"
+          >
+            {productData.name}
+          </Link>
+          {/* Item price */}
+          <p className="font-semibold">${productData.current_price}</p>
+          {/* Item Date */}
+          <p className="text-gray flex-start gap-1.5">
+            {" "}
+            <MdOutlineCalendarMonth /> {formate_date(productData.created_at)}
+          </p>
+        </div>
+      </div>
+      <button className="px-7.5 py-3.5 font-semibold text-sm rounded-sm bg-orange-lite hover:bg-orange! hover:text-white">
+        Quick View
+      </button>
+    </div>
+  );
+};
 
-export default FavoriteItem
+const itemStyle = `
+p-5 py-7.5 rounded-sm border border-border w-full duration-500! hover:shadow-[2px_5px_10px_var(--color-gray-light)]
+flex-between gap-5 flex-wrap
+`;
+
+export default FavoriteItem;
--- src/Pages/Wishlist/Wishlist_page.jsx
+++ src/Pages/Wishlist/Wishlist_page.jsx
@@ -3,28 +3,47 @@ import { useLayoutEffect } from "react";
 import Our_banner from "../../Components/ui/our-banner/Our_banner";
 import ProductCard from "../../Components/product/product-item/ProductCard";
 import FavoriteItem from "./FavoriteItem";
+import { FaRegHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Wishlist_page = () => {
   const { favoriteItems } = useFavoriteContext();
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
   });
+
+  console.log(favoriteItems);
   return (
     <div className="wishlist">
       <div className="container">
         <Our_banner page_data={{ title: "Wishlist" }} />
 
-        <div className="container">
+        {favoriteItems.length > 0 && (
           <div className="w-full">
-            {favoriteItems.map((pro) => {
-              return (
-                <div key={pro.id}>
-                  <FavoriteItem />
-                </div>
-              );
-            })}
+            <div className="w-full py-20 flex-start-col gap-5">
+              {favoriteItems.map((item) => {
+                return <FavoriteItem key={item.id} productData={item} />;
+              })}
+            </div>
           </div>
-        </div>
+        )}
+        {favoriteItems.length <= 0 && (
+          <div className="py-20 flex-center-col gap-5">
+            <div className="w-30 h-30 text-5xl bg-orange-lite text-orange/60 rounded-full flex-center">
+              <FaRegHeart />
+            </div>
+            <h1 className="text-2xl font-semibold">Your Wishlist Is Empty</h1>
+            <p className="text-sm text-gray">
+              Looks like you haven't added anything in your wishlist yet.
+            </p>
+            <Link
+              to={"/shop"}
+              className="px-7.5 py-3.5 rounded-sm bg-orange-lite font-semibold hover:bg-orange! hover:text-white! "
+            >
+              Start Shopping
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
