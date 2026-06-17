d96b51023e79919253728910971f7aec3e0f59aa
Ahmed Ibrahiem <ahmedebrihem935@gmail.com>
2026-06-17 17:35:15 +0300

    refector: re-design loading screen

--- src/Components/ui/Loading/Loading.jsx
+++ src/Components/ui/Loading/Loading.jsx
@@ -2,9 +2,14 @@ import style from "./Loading.module.css";
 
 export const Loading = () => {
   return (
-    <div className={style.loading}>
-      <img src="/assest/images/loading.png" loading="lazy"  alt="" />
-      <p>Loading...</p>
+    <div className="fixed w-full h-screen top-0 left-0 bg-white flex-center">
+      <div class={style.container}>
+        <div class={style.box}></div>
+        <div class={style.box}></div>
+        <div class={style.box}></div>
+        <div class={style.box}></div>
+        <div class={style.box}></div>
+      </div>
     </div>
   );
 };
